902dd60615f29a40b879cd68fb0639a72ff60c24
lachauxb <62470906+lachauxb@users.noreply.github.com>
2020-05-22 22:51:44 +0200

    Add files via upload

---
+++ conformite.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "httpparser.h"
+#include "api.h"
+#include "erreur.h"
+#include "request.h"
+#include "conformite.h"
+#include "reqtarget.h"
+
+
+int conformite_methode(_Token* root, int Id){
+	_Token *r, *r1, *r2, *r3, *r4;
+	r = searchTree(root,"method");
+	Lnode* noeud = (Lnode*) r->node;
+	if((compare_chaine(noeud->value,noeud->len,"GET",3)==0) || (compare_chaine(noeud->value,noeud->len,"HEAD",4)==0)){
+		r1 = searchTree(root,"message_body");
+		Lnode* n = (Lnode*) r1->node;
+		char* val = n->value;
+		if(val[0] != '\0'){
+			writeDirectClient(Id,ERROR400,strlen(ERROR400));
+			endWriteDirectClient(Id);
+			requestShutdownSocket(Id);
+			return 0;
+		}else{
+			return 1;
+		}
+	}
+	if(compare_chaine(noeud->value,noeud->len,"POST",4)==0){
+		r2 = searchTree(root,"message_body");
+		Lnode* n2 = (Lnode*) r2->node;
+		char* val2 = n2->value;
+		if(val2[0] == '\0'){
+			writeDirectClient(Id,ERROR400,strlen(ERROR400));
+			endWriteDirectClient(Id);
+			requestShutdownSocket(Id);
+			return 0;
+		}else{
+			r3 = searchTree(root,"Content_Length");
+			if(r3 == NULL){
+				r4 = searchTree(root,"Transfer_Encoding");
+				if(r4 == NULL){
+					writeDirectClient(Id,ERROR400,strlen(ERROR400));
+					endWriteDirectClient(Id);
+					requestShutdownSocket(Id);
+					return 0;
+				}else{
+					return 1;
+				}
+
+			}else{
+				Lnode* longueur_mb = (Lnode*) r2->node;
+				Lnode* longueur_CL = (Lnode*) r3->node;
+				int len = longueur_mb->len;
+				char* value = longueur_CL->value;
+				if(atoi(value) >= TAILLE_MB){
+					writeDirectClient(Id,ERROR413,strlen(ERROR413));
+					endWriteDirectClient(Id);
+					requestShutdownSocket(Id);
+				}
+				else if(len == atoi(value)){
+						return 1;
+				}else{
+					writeDirectClient(Id,ERROR400,strlen(ERROR400));
+					endWriteDirectClient(Id);
+					requestShutdownSocket(Id);
+					return 0;
+				}
+			}
+		}
+	}
+	if((compare_chaine(noeud->value,noeud->len,"PUT",3)!=0) && (compare_chaine(noeud->value,noeud->len,"DELETE",6)!=0) && (compare_chaine(noeud->value,noeud->len,"OPTIONS",7)!=0) && (compare_chaine(noeud->value,noeud->len,"TRACE",5)!=0) && (compare_chaine(noeud->value,noeud->len,"CONNECT",7)!=0) ){
+		writeDirectClient(Id,ERROR400,strlen(ERROR400));
+		endWriteDirectClient(Id);
+		requestShutdownSocket(Id);
+		return 0;
+	}
+}
+
+
+int compare_chaine(char *s1, int l1, char *s2, int l2){
+	int i = 0;
+	while(i<l1 && i<l2){
+		if(s1[i] != s2[i]) return -1;
+		else i++;
+	}
+	if(i == l1 && i == l2) return 0;
+	else return -1;
+}
+
+
+int conformite_version(_Token* root, int Id){
+  _Token *r, *r1;
+  r = searchTree(root,"HTTP_version");
+  Lnode* noeud = (Lnode*) r->node;
+  if(compare_chaine(noeud->value,noeud->len,"HTTP/1.0",8)==0){
+    return 1;
+   }
+  else if(compare_chaine(noeud->value,noeud->len,"HTTP/1.1",8)==0){
+      r1 = searchTree(root,"Host");
+      if(r1){
+      	return 1;
+      }else{
+        writeDirectClient(Id,ERROR400,strlen(ERROR400));
+        endWriteDirectClient(Id);
+        requestShutdownSocket(Id);
+        return 0;
+        }
+    } else {
+      writeDirectClient(Id,ERROR400,strlen(ERROR400));
+      endWriteDirectClient(Id);
+      requestShutdownSocket(Id);
+      return 0;
+    }
+}
+
+
+
+int unicite_header(_Token* root){
+	_Token *r,*r1;
+	r = searchTree(root,"header_field");
+	while (r) {	
+		int l;
+		char *s = getElementValue(r->node,&l);
+		char *str = malloc(l*sizeof(char));
+		int i=0 ;
+		while(i<l && s[i] != ':'){
+			if(s[i] == '-') { str[i] = '_'; } 
+			else { str[i] = s[i]; }
+			i++;
+		}
+		str[i] = '\0';
+		r1 = searchTree(root,str);
+		if(r1->next){ return 0; }
+		else { r=r->next; }
+	}
+	return 1;
+}
+
+
+
+int conformite(_Token *root, int Id){
+	if(unicite_header(root) == 1 && conformite_version(root,Id) == 1 && conformite_methode(root,Id) == 1) return 1;
+	else return -1;
+}
